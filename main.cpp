335c66f88ff9984f63e162951572631f44e69e3d
denan <dpoturak1@etf.unsa.ba>
2025-10-29 19:12:59 +0100

    first version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
+//-------------------MinHeap--------------------//
 template<typename T>
 class MinHrpa{
 private:
@@ -44,20 +46,79 @@ public:
   }
 };
 
-template<typename T2>
+//-------------------Graf--------------------//
 class Graf{
 private:
-  std::vector<std::vector<std::pair<int, T2>>> cvorovi;
+  std::vector<std::vector<std::pair<int, double>>> cvorovi;
 public:
-  Graf(std::vector<std::vector<std::pair<int, T2>>> svi) {
+  Graf(std::vector<std::vector<std::pair<int, double>>> svi) {
     cvorovi = svi;
   }
   void ShortestPathBinaryHeap(int cvor1, int cvor2) {
+    std::vector<std::pair<bool,double>> posjecen(cvorovi.size(), {false, std::numeric_limits<double>::infinity()});
+    MinHrpa<double> hrpa;
 
+    int trenutniCvor = cvor1;
+    posjecen[trenutniCvor] = {true, 0};
+
+    while(posjecen[cvor2].first == false){
+      for(int i = 0; i < cvorovi[trenutniCvor].size(); i++) {
+        hrpa.insert(cvorovi[trenutniCvor][i].first, posjecen[trenutniCvor].second+cvorovi[trenutniCvor][i].second);
+      }
+      std::pair<int, double> minimalni = hrpa.getMin();
+      if(posjecen[minimalni.first].first == false) {
+        posjecen[minimalni.first] = {true, minimalni.second};
+        trenutniCvor = minimalni.first;
+      }else {
+        while(posjecen[minimalni.first].first == true)
+          minimalni = hrpa.getMin();
+        posjecen[minimalni.first] = {true, minimalni.second};
+        trenutniCvor = minimalni.first;
+      }
+    }
+    std::cout << "Najkraci potevi: ";
+    for(int i = 0; i < posjecen.size(); i++)
+      std::cout <<posjecen[i].second << " ";
+    std::cout << "\n";
   }
 };
 int main()
 {
+  std::vector<std::vector<std::pair<int, double>>> listaSusjedstva = {
+      { {1, 1}, {2, 3}, {3, 6}, {4, 10}, {5, 15} },
+      { {0, 1}, {2, 2}, {3, 5}, {4, 9}, {5, 14} },
+      { {0, 3}, {1, 2}, {3, 3}, {4, 7}, {5, 12} },
+      { {0, 6}, {1, 5}, {2, 3}, {4, 4}, {5, 9} },
+      { {0, 10}, {1, 9}, {2, 7}, {3, 4}, {5, 5} },
+      { {0, 15}, {1, 14}, {2, 12}, {3, 9}, {4, 5} }
+  };
+  Graf mojGraf(listaSusjedstva);
+  mojGraf.ShortestPathBinaryHeap(0,5);
 
+  std::vector<std::vector<std::pair<int, double>>> listaSusjedstva2 = {
+    // Čvor 0: -> 1 (težina 3), -> 2 (težina 5)
+    { {1, 3}, {2, 5} },
+
+    // Čvor 1: -> 3 (težina 2), -> 4 (težina 4)
+    { {3, 2}, {4, 4} },
+
+    // Čvor 2: Nema direktnih izlaznih grana (osim samog sebe)
+    { },
+
+    // Čvor 3: -> 2 (težina 7), -> 4 (težina 2), -> 5 (težina 3)
+    { {2, 7}, {4, 2}, {5, 3} },
+
+    // Čvor 4: -> 6 (težina 2)
+    { {6, 2} },
+
+    // Čvor 5: -> 6 (težina 4)
+    { {6, 4} },
+
+    // Čvor 6: Nema direktnih izlaznih grana (osim samog sebe)
+    { }
+  };
+  std::cout << "Test 2:\n";
+  Graf mojGraf2(listaSusjedstva2);
+  mojGraf2.ShortestPathBinaryHeap(0,6);
   return 0;
 }
