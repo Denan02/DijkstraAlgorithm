98f28cf5583d28165649fbaf8d935bc176bd3b64
denan <dpoturak1@etf.unsa.ba>
2025-10-29 15:06:15 +0100

    pair

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
 
+template<typename T>
 class MinHrpa{
 private:
-  std::vector<double> niz;
+  std::vector<std::pair<int, T>> niz;
 public:
-  MinHrpa() {
-    niz = std::vector<double>(0);
-  }
-  void insert(double x) {
-    niz.push_back(x);
+  void insert(int cvor, T x) {
+    niz.push_back({cvor, x});
     int i = niz.size()-1;
     while(true) {
       int temp = (i-1)/2;
-      if(i-1 < 0 || niz[temp] < niz[i]) {
+      if(i-1 < 0 || niz[temp].second < niz[i].second) {
         break;
       }else {
         std::swap(niz[i], niz[temp]);
@@ -21,10 +19,10 @@ public:
       }
     }
   }
-  double getMin() {
+  std::pair<int, T> getMin() {
     if(niz.size() == 0)
       throw std::domain_error("Hrpa je prazana!");
-    double minBroj = niz[0];
+    std::pair<int, T> minBroj = niz[0];
     niz[0] = niz.back();
     niz.pop_back();
     this->Heapify(0);
@@ -36,49 +34,30 @@ public:
     int left = 2*i + 1;
     int right = 2*i + 2;
 
-    if(left < n && niz[left] < niz[smallest]) smallest = left;
-    if(right < n && niz[right] < niz[smallest]) smallest = right;
+    if(left < n && niz[left].second < niz[smallest].second) smallest = left;
+    if(right < n && niz[right].second < niz[smallest].second) smallest = right;
 
     if(smallest != i) {
         std::swap(niz[i], niz[smallest]);
         this->Heapify(smallest);
     }
   }
-  void ispisi() {
-    for(int i = 0; i < niz.size(); i++) {
-      std::cout << niz[i] << ", ";
-    }
-  }
 };
 
+template<typename T2>
+class Graf{
+private:
+  std::vector<std::vector<std::pair<int, T2>>> cvorovi;
+public:
+  Graf(std::vector<std::vector<std::pair<int, T2>>> svi) {
+    cvorovi = svi;
+  }
+  void ShortestPathBinaryHeap(int cvor1, int cvor2) {
+
+  }
+};
 int main()
 {
-  MinHrpa m = MinHrpa();
-  m.insert(2);
-  m.insert(3);
-  m.insert(10);
-  m.insert(4);
-  m.insert(5);
-  m.insert(1);
-  m.ispisi();
-  std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-  std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-    std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-  std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-    std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-    std::cout << "\n";
-  std::cout << m.getMin() << " ";
-  m.ispisi();
-    std::cout << "\n";
+
   return 0;
 }
